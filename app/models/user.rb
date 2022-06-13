c6acf858237957ac1cb147d7a1b8a47916e6529e
BaranovSev <aldebaran94@mail.ru>
2022-06-13 16:46:21 +0300

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token # non DB attribute of User class
+
   before_save { self.email = email.downcase } # Active Record Callback 
   
   validates :name, 
@@ -19,9 +21,33 @@ class User < ApplicationRecord
     presence: true, 
     length: { minimum: 6 }
   
+  # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
     BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns a random token. Used in theme 'permanent cookies'
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    # using 'self' -> concrete obj User has remember token
+    update_attribute(:remember_digest, User.digest(remember_token))
+    # update DB with encrypted token
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forget user
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_03_163631) do
+ActiveRecord::Schema.define(version: 2022_06_12_182643) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "password_digest"
+    t.string "password_digest" # hashed password
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
